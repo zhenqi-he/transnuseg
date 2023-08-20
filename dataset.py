6d581e40dda0d7c409cc300ae14889d4724f2871
ZhenqiHe <72725463+zhenqi-he@users.noreply.github.com>
2023-08-20 20:37:15 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -12,83 +12,12 @@ from torchvision import transforms
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 
+
 class MyDataset(Dataset):
-    def __init__(self,num_classes=2):
-        self.DATA_PATH = "/root/Swin_unet/dataset/"
-        self.IMG_DATA_PATH = os.path.join(self.DATA_PATH,"image.mat") 
-        self.MASK_DATA_PATH = os.path.join(self.DATA_PATH,"mask.mat") 
-        self.nems = np.load(os.path.join(self.DATA_PATH,"nem.npy"))
-        self.cems = np.load(os.path.join(self.DATA_PATH,"cem.npy"))
-        self.masks = sio.loadmat(self.MASK_DATA_PATH)['gt']
-        self.imgs = sio.loadmat(self.IMG_DATA_PATH)['imgs']
-    
-    def sem2ins(self,label):
-        seg_mask_g = label.copy()
-        
-        seg_mask_g[seg_mask_g != 0] = 1
-        seg_mask_g[seg_mask_g != 1] = 0
-        
-        contours, hierarchy = cv2.findContours(seg_mask_g, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
-        for i in range(len(contours)):
-            cnt = contours[i]
-            seg_mask_g = cv2.drawContours(seg_mask_g, [cnt], 0, i+1, -1)
-        return seg_mask_g
-
-    def __getitem__(self, index):
-        img = torch.tensor(np.uint8(self.imgs[:,:,index])).to(device)
-        mask = self.masks[:,:,index]
-
-        semantic_seg_mask = self.masks[:,:,index]
-        semantic_seg_mask[semantic_seg_mask > 0] = 1
-        semantic_seg_mask = torch.tensor(semantic_seg_mask).to(device)
-        semantic_seg_mask_ct = semantic_seg_mask.clone()
-        semantic_seg_mask_ct[semantic_seg_mask_ct>0] = -1
-        semantic_seg_mask_ct[semantic_seg_mask_ct==0] = 1
-        semantic_seg_mask_ct[semantic_seg_mask_ct==-1]=0
-
-
-
-        normal_edge_mask = self.nems[index]
-
-        normal_edge_mask_ct = normal_edge_mask.copy()
-        normal_edge_mask_ct[normal_edge_mask_ct>0] = -1
-        normal_edge_mask_ct[normal_edge_mask_ct==0] = 1
-        normal_edge_mask_ct[normal_edge_mask_ct==-1]=0
-
-
-
-        cluster_edge_mask = self.cems[index]
-        cluster_edge_mask_ct = cluster_edge_mask.copy()
-        cluster_edge_mask_ct[cluster_edge_mask_ct>0] = -1
-        cluster_edge_mask_ct[cluster_edge_mask_ct==0] = 1
-        cluster_edge_mask_ct[cluster_edge_mask_ct==-1]=0
-
-        instance_seg_mask = self.masks[:,:,index].copy() - cluster_edge_mask.copy()
-        instance_seg_mask = self.sem2ins(instance_seg_mask)
-        instance_seg_mask = torch.tensor(instance_seg_mask).to(device)
-
-
-        normal_edge_mask = torch.tensor(normal_edge_mask).to(device)
-        normal_edge_mask_ct = torch.tensor(normal_edge_mask_ct).to(device)
-
-
-        # cluster_edge_mask = self.generate_cluster_edge_mask(mask)
-        cluster_edge_mask = torch.tensor(cluster_edge_mask).to(device)
-        cluster_edge_mask_ct = torch.tensor(cluster_edge_mask_ct).to(device)
-
-        return img.unsqueeze(0), instance_seg_mask.unsqueeze(0),semantic_seg_mask.unsqueeze(0),normal_edge_mask.unsqueeze(0),cluster_edge_mask.unsqueeze(0)
-
-    def __len__(self):
-        return self.masks.shape[-1]
-
-
-class Histology(Dataset):
     '''
     dir_path: path to data, having two folders named data and label respectively
     '''
-    def __init__(self,dir_path,transform = None): 
+    def __init__(self,dir_path,transform = None,in_chan = 3): 
         self.dir_path = dir_path
         self.transform = transform
         self.data_path = os.path.join(dir_path,"data")
@@ -96,10 +25,15 @@ class Histology(Dataset):
         self.label_path = os.path.join(dir_path,"label")
         self.label_lists = sorted(glob.glob(os.path.join(self.label_path,"*.png")))
         
+        self.in_chan = in_chan
+        
     def __getitem__(self,index):
         img_path = self.data_lists[index]
         label_path = self.label_lists[index]
-        img = Image.open(img_path).convert("RGB")
+        if self.in_chan == 3:
+            img = Image.open(img_path).convert("RGB")
+        else:
+            img = Image.open(img_path).convert("L")
         label = cv2.imread(label_path)
         label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
         label_copy = label.copy()
@@ -138,19 +72,7 @@ class Histology(Dataset):
     
     def __len__(self):
         return len(self.data_lists)
-    def find_con(img,mask=None):
-        img = np.uint8(img)
-        num1, labels1 = cv2.connectedComponents(img)
-        labels1 = labels1.astype('uint8')
-        kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-        labels=cv2.dilate(labels1, kernel2, iterations=1)
 
-        #cv2.imshow('dst', labels.astype('uint8'))
-        #cv2.waitKey(0)
-        # if not os.path.exists(savepath):
-        #     os.mkdir(savepath)
-        # cv2.imwrite(os.path.join(savepath, mask), labels)
-        return labels
     
     def sem2ins(self,label):
         seg_mask_g = label.copy()
